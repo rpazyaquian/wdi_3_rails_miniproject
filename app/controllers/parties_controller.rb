1a06066c5722928a1bb72132269eb422337a422c
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-12 19:19:50 -0400

    slight change to parties controller

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,3 +1,16 @@
 class PartiesController < ApplicationController
-
+  def index
+  end
+  def create
+  end
+  def new
+  end
+  def edit
+  end
+  def show
+  end
+  def update
+  end
+  def destroy
+  end
 end
