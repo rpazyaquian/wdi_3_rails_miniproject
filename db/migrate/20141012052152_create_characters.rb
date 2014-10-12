4cd5148ec74272c94a9eec30ae4ca3cab9923678
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-12 01:28:24 -0400

    adding seeds, changed class attribute to job attribute (namespacing...........)

--- db/migrate/20141012052152_create_characters.rb
+++ db/migrate/20141012052152_create_characters.rb
@@ -2,7 +2,7 @@ class CreateCharacters < ActiveRecord::Migration
   def change
     create_table :characters do |t|
       t.string :name
-      t.string :class
+      t.string :job
       t.belongs_to :party, index: true
 
       t.timestamps null: false
