201e45500f25777d1dfd973aaa2180ab4758e9b5
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-12 01:22:07 -0400

    generated models for Character and Party

---
+++ db/migrate/20141012052058_create_parties.rb
@@ -0,0 +1,11 @@
+class CreateParties < ActiveRecord::Migration
+  def change
+    create_table :parties do |t|
+      t.string :name
+      t.string :goal
+      t.string :motto
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20141012052152_create_characters.rb
@@ -0,0 +1,11 @@
+class CreateCharacters < ActiveRecord::Migration
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :class
+      t.belongs_to :party, index: true
+
+      t.timestamps null: false
+    end
+  end
+end
