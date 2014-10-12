9c6e8229059f97f26accfc03260f386acb7d875f
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-12 14:22:43 -0400

    created database and schema, added new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 party = Party.create(name: "Munchkins", goal: "Loot literally everything", motto: "FBGMST")
 
-character = Character.create(name: 'Chunky', :class => :warrior)
+character = Character.create(name: 'Chunky', job: :tank)
+character = Character.create(name: 'Slashy', job: :dips)
+character = Character.create(name: 'Stabby', job: :dips)
+character = Character.create(name: 'Nukey', job: :dips)
+character = Character.create(name: 'Healbot', job: :healer)
