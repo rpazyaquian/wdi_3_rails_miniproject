266878f098717e9989877a50e74d54a2333dd04a
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-12 18:52:19 -0400

    character outputs job name with proper capitalization

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,12 @@
 class Character < ActiveRecord::Base
   belongs_to :party
+
+  def job
+    job = read_attribute(:job).to_s
+    if job == 'dps'
+      job.upcase
+    else
+      job.capitalize
+    end
+  end
 end
