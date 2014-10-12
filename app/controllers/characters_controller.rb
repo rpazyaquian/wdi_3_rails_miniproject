7c6fad7fdc25b7074c9ad36876e9b6cd811678e5
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-12 19:19:29 -0400

    defined typical CRUD methods for parties and characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,16 @@
 class CharactersController < ApplicationController
+  def index
+  end
+  def create
+  end
+  def new
+  end
+  def edit
+  end
+  def show
+  end
+  def update
+  end
+  def destroy
+  end
 end
