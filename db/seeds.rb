21c2460e8bd8e0e1f50b9afae2f8a2e8df9dab4f
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-12 14:40:42 -0400

    reorganized seeds to make sure Party model spec is testing the right thing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 munchkins = Party.create(name: "Munchkins", goal: "Loot literally everything", motto: "FBGMST")
 
-chunky = munchkins.characters.create(name: 'Chunky', job: :tank)
+healbot = munchkins.characters.create(name: 'Healbot', job: :healer)
 slashy = munchkins.characters.create(name: 'Slashy', job: :dips)
 stabby = munchkins.characters.create(name: 'Stabby', job: :dips)
 nukey = munchkins.characters.create(name: 'Nukey', job: :dips)
-healbot = munchkins.characters.create(name: 'Healbot', job: :healer)
+chunky = munchkins.characters.create(name: 'Chunky', job: :tank)
