771dfd9eefac0429c82eb41839bdcd34cb3b43e3
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-12 20:49:52 -0400

    moved form out into partial, return to party page on character add or delete

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,18 +1,17 @@
 class CharactersController < ApplicationController
   def create
     @character = Character.create(character_params)
-    redirect_to root_path
+    redirect_to :back
   end
   def destroy
     @character = Character.find(params[:id])
     @character.destroy
-    redirect_to root_path
+    redirect_to :back
   end
 
   private
-
     def character_params
-      params.require(:character).permit(:name, :job)
+      params.require(:character).permit(:name, :job, :party_id)
     end
 
 end
