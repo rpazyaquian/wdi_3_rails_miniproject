f594750c56ba81beb5ef9ec12637da25ab9e5d2f
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-14 11:03:37 -0400

    generated devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root "parties#index"
   resources :parties
   resources :characters, only: [:index, :show, :create, :edit, :update, :destroy]
