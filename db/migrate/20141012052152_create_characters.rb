370f93a6bbba1e954318298fea506eb5cbd20ec7
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-13 22:01:59 -0400

    forgot to add a comma before

--- db/migrate/20141012052152_create_characters.rb
+++ db/migrate/20141012052152_create_characters.rb
@@ -1,8 +1,8 @@
 class CreateCharacters < ActiveRecord::Migration
   def change
     create_table :characters do |t|
-      t.string :name null: false
-      t.string :job null: false
+      t.string :name, null: false
+      t.string :job, null: false
       t.belongs_to :party, index: true, null: false
 
       t.timestamps null: false
--- db/migrate/20141013023119_create_stat_sheets.rb
+++ db/migrate/20141013023119_create_stat_sheets.rb
@@ -1,12 +1,12 @@
 class CreateStatSheets < ActiveRecord::Migration
   def change
     create_table :stat_sheets do |t|
-      t.integer :hp null: false
-      t.integer :sp null: false
-      t.integer :attack null: false
-      t.integer :defense null: false
-      t.integer :speed null: false
-      t.belongs_to :character null: false
+      t.integer :hp, null: false
+      t.integer :sp, null: false
+      t.integer :attack, null: false
+      t.integer :defense, null: false
+      t.integer :speed, null: false
+      t.belongs_to :character, null: false
 
       t.timestamps null: false
     end
